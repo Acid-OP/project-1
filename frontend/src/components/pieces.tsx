07a11a4fe2ee76f05915acf9d002f145953273ea
Gaurav Kapur <gauravkapur596@gmail.com>
2025-02-06 03:05:51 +0530

    board done

---
+++ frontend/src/components/pieces.tsx
@@ -0,0 +1,11 @@
+export     const initialBoard = [
+        ["b_rook", "b_knight", "b_bishop", "b_queen", "b_king", "b_bishop", "b_knight", "b_rook"], // Black pieces
+        ["b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn", "b_pawn"], // Black pawns
+        [null, null, null, null, null, null, null, null], // Empty rows
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        ["w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn", "w_pawn"], // White pawns
+        ["w_rook", "w_knight", "w_bishop", "w_queen", "w_king", "w_bishop", "w_knight", "w_rook"], // White pieces
+      ];    
+      
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,39 +1,25 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import classNames from "classnames";
-
+import { initialBoard } from "../components/pieces";
 export function Home() {
-  const n = 8;
-  const m = 8;
-
-  const initialBoard = [
-    ["r", "n", "b", "q", "k", "b", "n", "r"], // Black pieces
-    ["p", "p", "p", "p", "p", "p", "p", "p"], // Black pawns
-    [null, null, null, null, null, null, null, null], // Empty rows
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null],
-    ["P", "P", "P", "P", "P", "P", "P", "P"], // White pawns
-    ["R", "N", "B", "Q", "K", "B", "N", "R"], // White pieces
-  ];
-
   const [chessBoard, setChessBoard] = useState<Array<Array<null | string>>>(initialBoard);
 
   return (
     <>
-      <div className="flex h-screen justify-center items-center">
-        <div className="border-4 border-solid border-black">
+      <div className="flex h-screen justify-center items-center bg-black">
+        <div className="border-1 border-white">
           {/* Render the chessboard */}
           {chessBoard.map((row, rIndex) => (
             <div className="flex" key={rIndex}>
               {row.map((piece, cIndex) => (
                 <div
                   className={classNames('w-12 h-12 flex items-center justify-center', {
-                    "bg-black": (rIndex + cIndex) % 2 === 0,
+                    "bg-[#8B4513]": (rIndex + cIndex) % 2 === 0,
                     "bg-white": (rIndex + cIndex) % 2 !== 0,
                   })}
                   key={cIndex}
                 >
-                  {piece && <span>{piece}</span>} {/* Display the piece here */}
+                  {piece && <img src={`/images/chess/${piece}.svg`} alt={piece} className="w-8 h-8" />}
                 </div>
               ))}
             </div>
